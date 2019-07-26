3fc1520d20e00e4f70c83b8796500d6ad76ab6ec
Anek <anekkumarsingh01@outlook.com>
2019-07-26 01:38:37 -0300

    read flavours

--- read.sh
+++ read.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+echo "==== reading without using any any variable ======="
 echo "Enter user name is :"
 read 
 echo "Entered values are : " ${REPLY}
+
+
+echo "============ Reading in the same line ========="
+
+read -p  "Hey enter your use name :" name1
+echo "entered :- " $name1
+
+echo "==== username and password example ========="
+read -p  "UserName : -" uname1
+read -sp "Password : -" pass1
+
+echo "UserName is :" $uname1
+echo "password is :" $pass1
